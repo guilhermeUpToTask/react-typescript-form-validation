2e47e8aade76f65cb3f7ea4b35c2e12ee1a84c3c
guilherme <guilherme256teretelteltel@gmail.com>
2023-05-11 08:35:07 -0300

    tryng to solve bug related to rendering react when consuming this project as a package

--- src/Components/UI/InvalidMesssage/index.tsx
+++ src/Components/UI/InvalidMesssage/index.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import {BiErrorCircle} from 'react-icons/bi';
 import classes from './index.module.css';
 interface  IInvalidProps {
     message: string;
 }
 
-export default function (props : IInvalidProps) {
+export default function (props : IInvalidProps) : React.ReactElement {
     return (
         <p className={classes.Invalid}>
             <BiErrorCircle /> {props.message}
